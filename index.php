3a29507d13d1af4ec64ec2b51031dfd34a2d5eaa
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-03-13 01:20:47 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,69 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>operador string - PHP</title>
+    <style>
+        body{
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h3>Operador string</h3>
+    <?php
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Operadores-Logicos - PHP</title>
-        <style>
-            body{
-                background-color: aqua;
-                text-align: center;
-            }
-        </style>
-    </head>
+    //Operador de concatenacao
+    $texto_a = "Olá ";
+    $texto_b = "Mundo!";
+    $texto_c = $texto_a . $texto_b;
 
-    <body>
-        <h3>Operadores-Logicos</h3>
+    echo ($texto_c); 
 
-        <?php
+    // a .= b'; é o mesmo que a = a . (+) b;  operador de incremento + concatenacao;
+    $texto_c .= $texto_b;
+    echo("<br> " . $texto_c);
 
-            var_dump(true || false);   echo " / ";
-            var_dump(true && true);    echo " / ";
-            var_dump(!true);  echo " / ";  // operador de negacao //
-            var_dump(true xor false);     /* Operador XOR retorna true se 
-                                         A ou B sao verdadeiros e nao AMBOS*/
+    $texto_c .= "mais alguma coisa";  //$texto_c = $texto_c . "mais alguma coisa";
+    echo("<br>" . $texto_c);
 
 
-            echo("<hr>");
-
-
-            $nome = "filipe";
-            $idade = 30;
-
-            if($nome == "filipe" && $idade == 32){
-                echo("Condicao verdadeira");
-            } else{
-                echo("Condicao Falsa");
-            };
-
-            echo("<hr>");
-
-            $cor_carro = "red";
-            $portas_carro = 2;
-
-            if($cor_carro == "blue" || $portas_carro == 2){
-                echo("Condicao verdadeira2");
-            }else{
-                echo("Condicao Falsa2");
-            }
-
-            echo("<hr>");
-
-            $idade2 = 18;
-            $cnh = true;
-
-            if($idade2 >= 18 && $cnh = true){
-                echo("Pode dirigir");
-            }else{
-                echo("Nao pode dirigir");
-            }
-
- 
-
-        ?>
-        
-    </body>
-
+    ?>
+    
+</body>
 </html>
